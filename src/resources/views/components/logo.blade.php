23f461b16cde07dbe41a6ba60faeb4e72191796b
CoconutRemco <99071124@mydavinci.nl>
2024-11-27 10:10:03 +0100

    Alle blades doorgelopen ze zijn allemaal individueel bruikbaar

--- src/resources/views/components/logo.blade.php
+++
@@ -1,5 +0,0 @@
-<a href="{{ route('website.home') }}" class="flex transition-all duration-200 hover:transform hover:scale-105">
-    <h1 class="font-bold">HARDEMAN</h1>
-    <span class="text-site-600">toelevering</span>
-    <span class="">.nl</span>
-</a>
